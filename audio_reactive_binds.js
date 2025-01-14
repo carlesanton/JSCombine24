9fa7cb256709863f5fc61c071e81ffcd408b3553
Carles Antón <carles.anton97@gmail.com>
2025-01-14 12:49:08 +0100

    feat: add audio_reactive_binds file to bind audio changes to module methods

---
+++ audio_reactive_binds.js
@@ -0,0 +1,15 @@
+import {change_ps_direction, disable_ps_direction_change_rate, set_ps_direction_change_rate_from_slider, set_ps_passes_per_frame_from_slider, disable_ps_passes_per_frame} from './lib/JSGenerativeArtTools/pixel_sort.js'
+import {set_ca_passes_per_frame_from_slider, disable_ca_passes_per_frame} from './lib/JSGenerativeArtTools/cellular_automata.js'
+import {level_scale, level_min_value, level_max_value, setOnBeatCallback, setOnLevelChangeCallback, setOnEnergyRatioChangeCallback} from './lib/JSGenerativeArtTools/audio/audio_reactive.js'
+
+var ps_speed_min_value = 2;
+var ps_speed_max_value = 23;
+
+function bind_audio_reactive_controls(){
+    console.log('Binding audio reactive controls')
+}
+
+
+export {
+    bind_audio_reactive_controls,
+}
