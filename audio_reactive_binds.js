4b5da83c6fe7036b68bef305f4a28b16a33741f7
Carles Antón <carles.anton97@gmail.com>
2025-01-23 20:49:59 +0100

    refactor: use audio reactive class instead of methods

--- audio_reactive_binds.js
+++ audio_reactive_binds.js
@@ -1,34 +1,40 @@
-import {change_ps_direction, disable_ps_direction_change_rate, set_ps_direction_change_rate_from_slider, set_ps_passes_per_frame_from_slider, disable_ps_passes_per_frame} from './lib/JSGenerativeArtTools/pixel_sort.js'
+import {change_ps_direction, disable_ps_direction_change_rate, set_ps_direction_change_rate_from_slider, set_ps_passes_per_frame_from_slider, disable_ps_passes_per_frame, defaultNoiseDirectionChangeRate} from './lib/JSGenerativeArtTools/pixel_sort.js'
 import {set_ca_passes_per_frame_from_slider, disable_ca_passes_per_frame} from './lib/JSGenerativeArtTools/cellular_automata.js'
-import {level_scale, level_min_value, level_max_value, setOnBeatCallback, setOnLevelChangeCallback, setOnEnergyRatioChangeCallback} from './lib/JSGenerativeArtTools/audio/audio_reactive.js'
+import {audioReactive} from './sketch.js'
 
 var ps_speed_min_value = 2;
 var ps_speed_max_value = 23;
+var level_max_value_ = 0.1;
 
 function bind_audio_reactive_controls(){
     console.log('Binding audio reactive controls')
 
     // Beat Detect
     console.log('Binding PS Direction to Beat Detection')
-    setOnBeatCallback(function() {
+    audioReactive.setOnBeatCallback(function() {
         change_ps_direction();
     });
-    set_ps_direction_change_rate_from_slider(0);
-    disable_ps_direction_change_rate();
+    audioReactive.addControllToTakeOver(
+        () => {
+            if (audioReactive.isAudioEnabled()){
+                set_ps_direction_change_rate_from_slider(0);
+            }
+            else {
+                set_ps_direction_change_rate_from_slider(defaultNoiseDirectionChangeRate);
+            }
+            disable_ps_direction_change_rate()
+        }
+    );
 
     // Audio Level
     console.log('Binding PS Speed to audio level')
-    setOnLevelChangeCallback(function(e) {
-        var scaled_level = level_scale * e;
-        var remapedForLog = map(scaled_level, level_min_value, level_max_value, 1, 2.8)
-        remapedForLog = constrain(remapedForLog, 1, 2.8)
-
-        var remapedLevel = map(log(remapedForLog), 0, 1, ps_speed_min_value, ps_speed_max_value)
-        remapedLevel = pow(remapedLevel, 1.3)
+    audioReactive.setOnLevelChangeCallback(function(e) {
+        let remapedLevel = audioReactive.mapLevel(e, audioReactive.levelScale)
+        remapedLevel = map(remapedLevel, 0, level_max_value_, ps_speed_min_value, ps_speed_max_value)
         remapedLevel = constrain(remapedLevel, ps_speed_min_value, ps_speed_max_value)
         set_ps_passes_per_frame_from_slider(remapedLevel);
     });
-    disable_ps_passes_per_frame();
+    audioReactive.addControllToTakeOver(disable_ps_passes_per_frame);
 
     // Centroid
     // console.log('Binding PS Speed to audio level')
@@ -41,7 +47,7 @@ function bind_audio_reactive_controls(){
 
     // Energy Ratio
     console.log('Binding CA Speed to MidHigh/Low energy ratio')
-    setOnEnergyRatioChangeCallback(function(energyRatio) {
+    audioReactive.setOnEnergyRatioChangeCallback(function(energyRatio) {
         var remapedRatio = map(energyRatio, 0, 0.3, 0, 5)
         remapedRatio = parseInt(constrain(remapedRatio, 0, 5))
         if (energyRatio<=0.1){ // To generate some CA movement without any audio
@@ -49,7 +55,7 @@ function bind_audio_reactive_controls(){
         }
         set_ca_passes_per_frame_from_slider(remapedRatio);
     });
-    disable_ca_passes_per_frame();
+    audioReactive.addControllToTakeOver(disable_ca_passes_per_frame);
 
     // HM Energy
     // // Discarted at the moment, using only the energy level of one band
