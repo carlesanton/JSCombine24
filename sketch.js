0c556d3966b5f704697b9c746c5077d2f7a2717c
Carles Antón <carles.anton97@gmail.com>
2026-01-26 21:15:52 +0100

    refactor: remove sketch comment

--- sketch.js
+++ sketch.js
@@ -9,11 +9,6 @@ import {bind_audio_reactive_controls} from './audio_reactive_binds.js'
 import { Recorder } from './lib/JSGenerativeArtTools/record/record.js';
 import { Mask } from './lib/JSGenerativeArtTools/mask/mask.js';
 
-// The desired artwork size in which everything is pixel perfect.
-// Let the canvas resize itself to fit the screen in "scaleCanvasToFit()" function.
-// Note that if the size is too small it will look blurry on bigger screens, that is why
-// we set "pixelDensity(4)" in this example (400x400 is pretty small).
-// If you target size is bigger you can reduce that value. e.g. "pixelDensity(2)".
 // Inputs
 // Main
 let MainInputs
